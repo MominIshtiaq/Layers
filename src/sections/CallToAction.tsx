e67a0f2b87996fe750c6bf90b04c5c0df9dd4955
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-04-29 13:26:59 +0500

    Faq and CallToAction animation complete

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,58 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import cursorYouImage from "@/assets/images/cursor-you.svg";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+
 export default function CallToAction() {
+    const [isHovered, setIsHovered] = useState(false);
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            { x: "-50%" },
+            {
+                duration: 30,
+                ease: "linear",
+                repeat: Infinity,
+            }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) {
+            if (isHovered) {
+                animation.current.speed = 0.3;
+            } else {
+                animation.current.speed = 1;
+            }
+        }
+    }, [isHovered]);
+
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+                <motion.div
+                    ref={scope}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group"
+                    style={{
+                        cursor: `url(${cursorYouImage.src}), auto`,
+                    }}
+                >
                     {Array.from({ length: 10 }).map((_, i) => (
                         <div key={i} className="flex items-center gap-16">
                             <span className="text-lime-400 text-7xl">
                                 &#10038;
                             </span>
-                            <span>Try it for free</span>
+                            <span className="group-hover:text-lime-400">
+                                Try it for free
+                            </span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
     );
--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Tag from "@/components/Tag";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const faqs = [
@@ -25,7 +28,7 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    const selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] = useState(0);
     return (
         <section className="py-24 ">
             <div className="container">
@@ -42,7 +45,10 @@ export default function Faqs() {
                             key={faq.question}
                             className="bg-neutral-900 rounded-2xl border border-white/10 p-6"
                         >
-                            <div className="flex justify-between items-center">
+                            <div
+                                className="flex justify-between items-center"
+                                onClick={() => setSelectedIndex(index)}
+                            >
                                 <h3 className="font-medium">{faq.question}</h3>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -55,7 +61,7 @@ export default function Faqs() {
                                     strokeLinecap="round"
                                     strokeLinejoin="round"
                                     className={twMerge(
-                                        "feather feather-plus text-lime-400 flex-shrink-0",
+                                        "feather feather-plus text-lime-400 flex-shrink-0 transition duration-300",
                                         selectedIndex === index && "rotate-45"
                                     )}
                                 >
@@ -63,14 +69,26 @@ export default function Faqs() {
                                     <line x1="5" y1="12" x2="19" y2="12"></line>
                                 </svg>
                             </div>
-                            <div
-                                className={twMerge(
-                                    "mt-6",
-                                    selectedIndex !== index && "hidden"
+                            <AnimatePresence>
+                                {selectedIndex === index && (
+                                    <motion.div
+                                        initial={{ height: 0, marginTop: 0 }}
+                                        animate={{
+                                            height: "auto",
+                                            marginTop: 24,
+                                        }}
+                                        exit={{
+                                            height: 0,
+                                            marginTop: 0,
+                                        }}
+                                        className={twMerge("overflow-hidden")}
+                                    >
+                                        <p className="text-white/50">
+                                            {faq.answer}
+                                        </p>
+                                    </motion.div>
                                 )}
-                            >
-                                <p className="text-white/50">{faq.answer}</p>
-                            </div>
+                            </AnimatePresence>
                         </div>
                     ))}
                 </div>
